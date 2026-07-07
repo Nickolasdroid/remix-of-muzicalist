4ca4131ba9d8a0619d94c314ec09a20651f87aa6
Nickolasdroid <catalinnapau69@gmail.com>
2026-07-07 22:22:17 +0300

    Add Android Capacitor app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.muzicalist.app',
+  appName: 'Muzicalist',
+  webDir: 'dist'
+};
+
+export default config;
